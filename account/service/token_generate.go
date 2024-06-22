481886e1564a93d849c683d6cae111b7f9363c6b
gorik <delavarus@gmail.com>
2024-06-22 20:53:20 +0300

    token_service_created

--- account/service/token_generate.go
+++ account/service/token_generate.go
@@ -4,6 +4,7 @@ import (
 	"crypto/rsa"
 	"github.com/golang-jwt/jwt/v5"
 	"github.com/google/uuid"
+	"log"
 	"memory-app/account/models"
 	"time"
 )
@@ -18,19 +19,73 @@ type RefreshTokenCustomClaims struct {
 	Uid uuid.UUID `json:"uid"`
 	jwt.RegisteredClaims
 }
+type IDTokenCustomClaims struct {
+	User *models.User `json:"user"`
+	jwt.RegisteredClaims
+}
 
 func generateRefreshToken(uid uuid.UUID, secret string) (*RefreshToken, error) {
-
+	//:::PRE setup
 	currentTime := time.Now()
-	tokenTime := currentTime.AddDate(0, 0, 3)
+	tokenExp := currentTime.AddDate(0, 0, 3)
 
 	tokenID, _ := uuid.NewRandom()
 
-	claims := RefreshTokenCustomClaims{}
+	//:::CLAIMS setup
+	customClaims := RefreshTokenCustomClaims{
+		Uid: uid,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(tokenExp),
+			IssuedAt:  jwt.NewNumericDate(currentTime),
+			ID:        tokenID.String(),
+		},
+	}
 
-	jwt.NewWithClaims()
+	//:::TOKEN setup
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, customClaims)
+	signedToken, err := token.SignedString(secret)
+
+	if err != nil {
+		log.Println("Failed to sign refresh token string")
+		return nil, err
+	}
+
+	//:::RETURN
+	return &RefreshToken{
+		SS:      signedToken,
+		ID:      tokenID.String(),
+		Expired: tokenExp.Sub(currentTime),
+	}, nil
 }
 
 func generateToken(u *models.User, key *rsa.PrivateKey) (string, error) {
-	return "", err
+	{
+		//:::PRE setup
+		currentTime := time.Now().Unix()
+		tokenExp := currentTime + 60*15
+
+		tokenID, _ := uuid.NewRandom()
+
+		//:::CLAIMS setup
+		customClaims := IDTokenCustomClaims{
+			User: u,
+			RegisteredClaims: jwt.RegisteredClaims{
+				ExpiresAt: jwt.NewNumericDate(time.Unix(tokenExp, 0)),
+				IssuedAt:  jwt.NewNumericDate(time.Unix(currentTime, 0)),
+				ID:        tokenID.String(),
+			},
+		}
+
+		//:::TOKEN setup
+		token := jwt.NewWithClaims(jwt.SigningMethodRS256, customClaims)
+		signedToken, err := token.SignedString(key)
+
+		if err != nil {
+			log.Println("Failed to sign refresh token string")
+			return "", err
+		}
+
+		//:::RETURN
+		return signedToken, nil
+	}
 }
